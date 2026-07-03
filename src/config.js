e265070ae5d0622b146cef2493dc8f676cf65d19
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2026-07-03 17:15:57 -0400

    - resolve npm issues/vulnerabilities

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 // src/config.js
-export const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID; 
+export const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 // Automatically switch redirect URI based on environment
-export const REDIRECT_URI = import.meta.env.MODE === 'production' 
+export const REDIRECT_URI = import.meta.env.MODE === 'production'
   ? "https://coleswinford.github.io/spotify-aggregator/"
-  : "http://127.0.0.1:5173/";
-  
+  : "http://127.0.0.1:5173/"; 
+
 export const SCOPES = "user-read-private playlist-read-private";
+// export const SCOPES = "user-read-private playlist-read-private streaming user-read-playback-state user-modify-playback-state";
